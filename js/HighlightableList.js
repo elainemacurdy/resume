8eb17a9e75b374b199c1b84d64954065b7f51c11
Elaine Harman <elaine.harman@audiencescience.com>
2017-01-17 16:40:20 -0500

    Updating with latest changes.

--- js/HighlightableList.js
+++ js/HighlightableList.js
@@ -1,22 +1,16 @@
 function HighlightableList(props) {
     this._container = props.container;
-    this._highlightTimeout = null;
     this._keywords = props.keywords;
-    this._timeoutMs = 500;
 }
-
-HighlightableList.prototype.cancelRequest = function(e) {
-    this._highlightTimeout = clearTimeout(this._highlightTimeout);
-};
 
 HighlightableList.prototype.generate = function() {
     this._keywords.forEach(function(keyword, i) {
         this._container.append($("<span/>")
-            .hover(function(e) { this.requestHighlight(e, keyword); }.bind(this), this.cancelRequest.bind(this))
+            .bind("click", function(e) { this.highlight(e, keyword); }.bind(this))
             .text(keyword)
         );
         if (i !== this._keywords.length - 1) {
-            this._container.append(', ');
+            this._container.append(", ");
         }
     }.bind(this));
     $(document.body).click(function() {
@@ -24,11 +18,10 @@ HighlightableList.prototype.generate = function() {
     });
 };
 
-HighlightableList.prototype.requestHighlight = function(e, term) {
-    this._highlightTimeout = setTimeout(function() {
-        this._highlightTimeout = clearTimeout(this._highlightTimeout);
-        $(document.body)
-            .unhighlight()
-            .highlight(term, { caseSensitive: true, wordsOnly: true });
-    }, this._timeoutMs);
+HighlightableList.prototype.highlight = function(e, term) {
+    e.stopPropagation();
+    console.log('highlight');
+    $(document.body)
+        .unhighlight()
+        .highlight(term, { caseSensitive: true, wordsOnly: true });
 };
